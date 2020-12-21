53ae914561171ea84eea18a717a8a93f179f1f0f
ALEXANDER MORTON <alexmorton.home@gmail.com>
2020-12-21 22:20:25 +1100

    this can finally scrape the menu site properly using selenium

--- bot.py
+++ bot.py
@@ -1,22 +1,30 @@
 
 import os, ssl
-
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
+from selenium import webdriver 
 
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)): #Not sure how this works but it does
     ssl._create_default_https_context = ssl._create_unverified_context
-
 
 my_url = 'https://www.odds.com.au/greyhounds/'
 
-uclient = urlopen(my_url)
-page_html = uclient.read()
-page_soup = soup(page_html, "html.parser")
-uclient.close()
+browser = webdriver.Chrome('/Users/alexmorton/opt/WebDriver/bin/chromedriver') #The chromedriver filepath needs to be correct
+browser.get(my_url) #This will open a browser with the URL
+
+
+## REDUNDANT CODE ##
+# uclient = urlopen(my_url)
+# page_html = uclient.read()
+# page_soup = soup(page_html, "html.parser")
+# uclient.close()
+
+html = browser.page_source # Use loaded page source on the browser and make html soup 
+page_soup = soup(html, "html.parser") 
 
 
 
-rows = page_soup.find_all("a", {"class": "event-lists__link"})
+
+rows = page_soup.find_all("div", {"class": "racing-meeting-row__header"})
 print(len(rows))
-print(rows)
+# print(rows)
