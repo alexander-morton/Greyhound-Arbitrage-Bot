b87a2f79f1da8ab50d784b836b7922cfbd2a5a4d
WILL MAGAREY <wmagarey@gmail.com>
2020-12-22 14:37:12 +1100

    zorp

--- oddscraper.py
+++ oddscraper.py
@@ -1,6 +1,7 @@
 import os, ssl
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
+from selenium import webdriver 
 
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)): #Not sure how this works but it does
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -8,8 +9,16 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unveri
 
 
 my_url = 'https://www.odds.com.au/greyhounds/ipswich/race-4/?date=2020-12-22'
-uclient = urlopen(my_url)
-page_html = uclient.read()
-uclient.close()
-
+browser = webdriver.Chrome('/Users/Bet-tings/chromedriver')
+browser.get(my_url)
+html = browser.page_source
 page_soup = soup(page_html, 'html.parser')
+
+
+rows = page_soup.find_all("span", {"class":"competitor-details"})
+print(len(rows))
+#dogs = []
+
+#for dog in rows:
+    #dogs.append(dog.getText())
+#print(dogs)
